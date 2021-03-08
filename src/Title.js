f4531fe941d3e3557d0c9caf9dcddc4ac6212922
Bry Lance <bry@riverbryanlance.net>
2021-03-08 11:09:20 -0500

    className

--- src/Title.js
+++ src/Title.js
@@ -2,7 +2,7 @@ export default function Title({title, month, day, year}) {
     return (
         <>
         <h1>{title}</h1>
-        <p class="date">Date: {month}/{day}/{year}</p>
+        <p className="date">Date: {month}/{day}/{year}</p>
         </>
     )
 }
